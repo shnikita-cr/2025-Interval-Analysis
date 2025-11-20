f1aeddf616d10296518de2b84e502f4ddfd54056
Nikita <professornikson@gmail.com>
2025-11-20 13:25:01 +0300

    fix: set global [A,B]

--- lab1/task/task.h
+++ lab1/task/task.h
@@ -6,10 +6,12 @@
 using DI = Interval<double>;
 using DIAM = AMatrix<DI>;
 
+const double A = 0, B = 1;
+
 DIAM getStartIntervalMatrix(double delta) {
     return {
             {DI(0.95 - delta, 0.95 + delta), DI(1.00 - delta, 1.00 + delta)},
-//            {DI(1.05 - delta, 1.05 + delta), DI(1.05 - delta, 1.05 + delta)},
+            {DI(1.05 - delta, 1.05 + delta), DI(1.05 - delta, 1.05 + delta)},
             {DI(1.10 - delta, 1.10 + delta), DI(1.10 - delta, 1.10 + delta)}
     };
 }
