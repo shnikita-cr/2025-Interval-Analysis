96d87ffea8ba8486b5c09f2ddef76a6c1e5296f7
Nikita <professornikson@gmail.com>
2025-11-23 13:28:40 +0300

    change: use formatted task

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -5,15 +5,26 @@
 
 using DI = Interval<double>;
 
+double f(double x) {
+    return f1(x);
+}
+
+double A = A1, B = B1;
+
+//double f(double x) {
+//    return f2(x);
+//}
+//
+//double A = A2, B = B2;
+
 int main() {
     std::cout << "main()" << std::endl;
     std::cout << DI(0) << std::endl;
 
     size_t n = 100;
     DVector x(n), y_f_linear(n), y_f_width(n);
-    double a = -10, b = 10;
-    vectorFillXValues(x, a, b, n);
-    vectorFillFValues(y_f_linear, a, b, n, f1_linear);
+    vectorFillXValues(x, A, B, n);
+    vectorFillFValues(y_f_linear, A, B, n, f);
 
     vectorToFile(x, "../../data/x_h.txt");
     vectorToFile(y_f_linear, "../../data/y_f_linear.txt");
