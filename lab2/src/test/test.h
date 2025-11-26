39d8e48aed0a63565c95932e629fb26f42221007
Nikita <professornikson@gmail.com>
2025-11-27 01:13:40 +0300

    refactor: format test output, add <= out

--- lab2/src/test/test.h
+++ lab2/src/test/test.h
@@ -26,8 +26,9 @@ void test_task(const Task &task) {
     std::cout << "estimate_value_B1_dist = " << estimate_value_B1_dist << std::endl;
     double left_B1 = estimate_value_B1.getRad();
     double right_B1 = L * X.getRad();
-    std::cout << "left = " << left_B1 << "right = " << right_B1 << std::endl;
+    std::cout << "left = " << left_B1 << "; right = " << right_B1 << std::endl;
     std::cout << "right - left = " << right_B1 - left_B1 << std::endl;
+    std::cout << "left <= right " <<  (left_B1 <= right_B1) << std::endl;
     std::cout << "inaccuracy = " << estimate_value_B1.getWidth() << std::endl;
 
     //B2
@@ -39,8 +40,9 @@ void test_task(const Task &task) {
     std::cout << "estimate_value_B1_dist = " << estimate_value_B2_dist << std::endl;
     double left_B2 = estimate_value_B2.getRad();
     double right_B2 = L * X.getRad();
-    std::cout << "left = " << left_B2 << "right = " << right_B2 << std::endl;
+    std::cout << "left = " << left_B2 << "; right = " << right_B2 << std::endl;
     std::cout << "right - left = " << right_B2 - left_B2 << std::endl;
+    std::cout << "left <= right " <<  (left_B2 <= right_B2) << std::endl;
     std::cout << "inaccuracy = " << estimate_value_B2.getWidth() << std::endl;
 
     //B3
@@ -52,8 +54,9 @@ void test_task(const Task &task) {
     std::cout << "estimate_value_B1_dist = " << estimate_value_B3_dist << std::endl;
     double left_B3 = estimate_value_B3.getRad();
     double right_B3 = L * X.getRad();
-    std::cout << "left = " << left_B3 << "right = " << right_B3 << std::endl;
+    std::cout << "left = " << left_B3 << "; right = " << right_B3 << std::endl;
     std::cout << "right - left = " << right_B3 - left_B3 << std::endl;
+    std::cout << "left <= right " <<  (left_B3 <= right_B3) << std::endl;
     std::cout << "inaccuracy = " << estimate_value_B3.getWidth() << std::endl;
 
     //B4
@@ -65,8 +68,9 @@ void test_task(const Task &task) {
     std::cout << "estimate_value_B1_dist = " << estimate_value_B4_dist << std::endl;
     double left_B4 = estimate_value_B4.getRad();
     double right_B4 = L * X.getRad();
-    std::cout << "left = " << left_B4 << "right = " << right_B4 << std::endl;
+    std::cout << "left = " << left_B4 << "; right = " << right_B4 << std::endl;
     std::cout << "right - left = " << right_B4 - left_B4 << std::endl;
+    std::cout << "left <= right " <<  (left_B4 <= right_B4) << std::endl;
     std::cout << "inaccuracy = " << estimate_value_B4.getWidth() << std::endl;
 
     std::cout << "===================B5===============================" << std::endl;
@@ -78,8 +82,9 @@ void test_task(const Task &task) {
     std::cout << "estimate_value_B1_dist = " << estimate_value_B5_dist << std::endl;
     double left_B5 = estimate_value_B5.getRad();
     double right_B5 = L * X.getRad();
-    std::cout << "left = " << left_B5 << "right = " << right_B5 << std::endl;
+    std::cout << "left = " << left_B5 << "; right = " << right_B5 << std::endl;
     std::cout << "right - left = " << right_B5 - left_B5 << std::endl;
+    std::cout << "left <= right " <<  (left_B5 <= right_B5) << std::endl;
     std::cout << "inaccuracy = " << estimate_value_B5.getWidth() << std::endl;
 
 }
