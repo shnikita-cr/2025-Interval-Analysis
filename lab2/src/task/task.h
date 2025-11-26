d3edcba743812483dd5ed5fdb745ff2b9141028a
Nikita <professornikson@gmail.com>
2025-11-26 15:59:28 +0300

    add: struct for task

--- lab2/src/task/task.h
+++ lab2/src/task/task.h
@@ -16,3 +16,15 @@ double f2(double x) {
 }
 
 double A2 = 0, B2 = 100;
+
+uint32_t DOT_COUNT = 300;
+
+struct Task {
+    double A, B;
+
+    double (*f)(double);
+
+    uint32_t n;
+
+    Task(double a, double b, double (*f)(double), uint32_t n) : A(a), B(b), f(f), n(n) {}
+};
