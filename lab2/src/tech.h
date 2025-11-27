7e493e4e1481a837075f4f38e240864d7755f214
Nikita <professornikson@gmail.com>
2025-11-27 03:02:17 +0300

    add: printAll

--- lab2/src/tech.h
+++ lab2/src/tech.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <iostream>
+
+template<typename... Args>
+void printAll(Args... args) {
+    (void) std::initializer_list<int>{((std::cout << args << " "), 0)...};
+    std::cout << std::endl;
+}
