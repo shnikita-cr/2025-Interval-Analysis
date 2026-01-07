3b76f75ca2bf7664284188f0c1e07a1901df9cc5
Nikita <59667583+shnikita-cr@users.noreply.github.com>
2026-01-07 15:49:54 +0300

    add: task0 from book

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -2,11 +2,13 @@
 #include "task/task.h"
 #include "task/task_values.h"
 
+Task task0{A0, b0, {{0, 20}, {0, 20}}, 0};
 Task task1{A1, b1, {{-10, 10}, {-10, 10}}, 1};
 Task task2{A2, b2, {{-10, 10}, {-10, 10}}, 2};
 Task task3{A3, b3, {{-10, 10}, {-10, 10}}, 3};
 
 int main() {
+//    test_task(task0);
     test_task(task1);
 //    test_task(task2, 2);
 //    test_task(task3, 3);
