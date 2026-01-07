f6a1cf294bd7d12003089add14c8d754e5c4a49c
Nikita <59667583+shnikita-cr@users.noreply.github.com>
2026-01-07 23:43:19 +0300

    change: main enable all tests, use new func descr

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -1,6 +1,7 @@
 #include "test/test.h"
 #include "task/task.h"
 #include "task/task_values.h"
+#include "test/test_intervals.h"
 
 Task task0{A0, b0, {{0, 20}, {0, 20}}, 0};
 Task task1{A1, b1, {{-10, 10}, {-10, 10}}, 1};
@@ -8,9 +9,10 @@ Task task2{A2, b2, {{-10, 10}, {-10, 10}}, 2};
 Task task3{A3, b3, {{-10, 10}, {-10, 10}}, 3};
 
 int main() {
-//    test_task(task0);
+//    test_im();
+    test_task(task0);
     test_task(task1);
-//    test_task(task2, 2);
-//    test_task(task3, 3);
+    test_task(task2);
+    test_task(task3);
     return 0;
 }
