147be877507163c564073eff0bd989154c741b86
Nikita <59667583+shnikita-cr@users.noreply.github.com>
2026-01-07 15:50:06 +0300

    fix: log to console (temp)

--- lab3/src/secondary/flogger.h
+++ lab3/src/secondary/flogger.h
@@ -1,4 +1,5 @@
 #include "logger.h"
 
-static auto logFile = std::ofstream("../log/log.txt");
-static Logger flogger(logFile);
+//static auto logFile = std::ofstream("../log/log.txt");
+//static Logger flogger(logFile);
+static Logger flogger(std::cout);
